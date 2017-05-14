e2d0336cecdd3f0f335cf9e57e71255d8cf42269
wyxccs <wyxccs@163.com>
2017-05-14 09:35:45 +0800

    Conversion system init

---
+++ CType/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CType
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cb = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textb = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cb
+            // 
+            this.cb.FormattingEnabled = true;
+            this.cb.Items.AddRange(new object[] {
+            "16-10",
+            "16-8",
+            "16-2",
+            "10-16",
+            "10-8",
+            "10-2",
+            "8-16",
+            "8-10",
+            "8-2",
+            "2-16",
+            "2-10",
+            "2-8"});
+            this.cb.Location = new System.Drawing.Point(101, 23);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(128, 20);
+            this.cb.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "转换方式";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "源数据";
+            // 
+            // textb
+            // 
+            this.textb.Location = new System.Drawing.Point(101, 71);
+            this.textb.Name = "textb";
+            this.textb.Size = new System.Drawing.Size(128, 21);
+            this.textb.TabIndex = 3;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(101, 123);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(128, 21);
+            this.result.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "转换结果";
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(120, 175);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 6;
+            this.btn.Text = "点击转换";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Location = new System.Drawing.Point(226, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "猫熊出品";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 252);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.textb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "进制转换工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textb;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CType/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CType
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cb.Text = "16-10";
+            cb.SelectedIndex = 0;
+            cb.Text = "16-8";
+            cb.SelectedIndex = 1;
+            cb.Text = "16-2";
+            cb.SelectedIndex = 2;
+
+            cb.Text = "10-16";
+            cb.SelectedIndex = 3;
+            cb.Text = "10-8";
+            cb.SelectedIndex = 4;
+            cb.Text = "10-2";
+            cb.SelectedIndex = 5;
+
+            cb.Text = "8-16";
+            cb.SelectedIndex = 6;
+            cb.Text = "8-10";
+            cb.SelectedIndex = 7;
+            cb.Text = "8-2";
+            cb.SelectedIndex = 8;
+
+            cb.Text = "2-16";
+            cb.SelectedIndex = 9;
+            cb.Text = "2-10";
+            cb.SelectedIndex = 10;
+            cb.Text = "2-8";
+            cb.SelectedIndex = 11;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string text = textb.Text;
+            if (text.Equals(""))
+            {
+                MessageBox.Show("请输入参数");
+                goto cc;
+            }
+            
+            try {
+                switch (cb.SelectedIndex)
+                {
+                    case 0:
+                        result.Text = Convert.ToInt32(text, 16) + "";
+                        break;
+                    case 1:
+                        result.Text= Convert.ToString(Convert.ToInt32(text, 16), 8);
+                        break;
+                    case 2:
+                        result.Text = Convert.ToString(Convert.ToInt32(text, 16), 2);
+                        break;
+                    case 3:
+                        result.Text = Convert.ToString(int.Parse(text), 16);
+                        break;
+                    case 4:
+                        result.Text = Convert.ToString(int.Parse(text), 8);
+                        break;
+                    case 5:
+                        result.Text = Convert.ToString(int.Parse(text), 2);
+                        break;
+                    case 6:                    
+                        result.Text = Convert.ToString(Convert.ToInt32(text, 8), 16);
+                        break;
+                    case 7:
+                        result.Text= Convert.ToInt32(text, 8)+"";
+                        break;
+                    case 8:
+                        result.Text = Convert.ToString(Convert.ToInt32(text, 8),2);
+                        break;
+                    case 9:
+                        result.Text = Convert.ToString(Convert.ToInt32(text, 2), 16);
+                        break;
+                    case 10:
+                        result.Text = Convert.ToInt32(text, 2)+"";
+                        break;
+                    case 11:
+                        result.Text = Convert.ToString(Convert.ToInt32(text, 2), 8);
+                        break;
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+                MessageBox.Show("转换出错");
+            }
+
+
+             cc:;
+
+        }
+    }
+}
